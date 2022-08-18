e1d4073fd5fb93f56479b6a2f3e9a497c2104491
Anne Ogongo <anneogongo98@gmail.com>
2022-08-18 17:21:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return (customerName = customerName.toUpperCase());
+};
+function setBestCustomer() {
+    bestCustomer = "not bob";
+};
+function overwriteBestCustomer(name) {
+    bestCustomer = name;
+};
+const leastFavoriteCustomer = "Kate";
+
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = "she";
+}
